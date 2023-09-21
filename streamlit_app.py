b9eae2bbdc4bba7488a67c2993b88605de6b080e
jcplemmons <145688913+jcplemmons@users.noreply.github.com>
2023-09-21 16:29:26 -0600

    Update streamlit_app.py
    import snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,5 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
+
+import snowflake.connector
