6c61040cc98d53615f117772edd34cb5391ab5d0
jcplemmons <145688913+jcplemmons@users.noreply.github.com>
2023-09-21 08:53:24 -0600

    Update streamlit_app.py
    add picklist to smoothie menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,8 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+
+# Creat picklist so customers can pick the fruit they want.
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+#display the table in the page
+streamlit.dataframe(my_fruit_list)
